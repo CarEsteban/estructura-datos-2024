65f0faea10d0d838ed34e1dadcc1a9a27c188d71
CarEsteban <car23016@uvg.edu.gt>
2024-02-05 21:28:14 -0600

    antes de hacer las pruebas de profiler

--- HTD3/src/main.py
+++ HTD3/src/main.py
@@ -2,17 +2,19 @@ import random, sorts
 
 def generatorRandom():
     
-    array = [random.randint(1,10000) for j in range(3000)]
+    array = [random.randint(1,10000) for j in range(10)]
     
+
     return array
 
 numerosAleatorios = generatorRandom()
 
-#print(sorts.gnome_sort(numerosAleatorios))
-#print(sorts.heapSort(numerosAleatorios))
-#print(sorts.mergeSort(numerosAleatorios))
-#print(sorts.quickSort(numerosAleatorios,0,len(numerosAleatorios)-1))
-#print(sorts.radixSort(numerosAleatorios))
-#print(sorts.selectionSort(numerosAleatorios))
-print(sorts.shellSort(numerosAleatorios))
+if __name__ == "__main__":
+    print(sorts.quickSort(numerosAleatorios,0,len(numerosAleatorios)-1))
+    print(sorts.gnome_sort(numerosAleatorios))
+    print(sorts.heapSort(numerosAleatorios))
+    print(sorts.mergeSort(numerosAleatorios))
+    print(sorts.radixSort(numerosAleatorios))
+    print(sorts.selectionSort(numerosAleatorios))
+    print(sorts.shellSort(numerosAleatorios))
 
