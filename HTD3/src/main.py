df563afc4b025e80affc529ccd319c87cde38d84
CarEsteban <car23016@uvg.edu.gt>
2024-02-06 19:34:18 -0600

    main

--- HTD3/src/main.py
+++ HTD3/src/main.py
@@ -2,7 +2,7 @@ import random, sorts
 
 def generatorRandom():
     
-    array = [random.randint(1,10000) for j in range(10)]
+    array = [random.randint(1,10000) for j in range(4000)]
     
 
     return array
@@ -11,11 +11,11 @@ numerosAleatorios = generatorRandom()
 ascDescBoolean = False
 
 if __name__ == "__main__":
-    print(sorts.quickSort(numerosAleatorios,0,len(numerosAleatorios)-1,ascDescBoolean))
-    print(sorts.gnomeSort(numerosAleatorios,ascDescBoolean))
-    print(sorts.heapSort(numerosAleatorios,ascDescBoolean))
+    #print(sorts.quickSort(numerosAleatorios,0,len(numerosAleatorios)-1,ascDescBoolean))
+    #print(sorts.gnomeSort(numerosAleatorios,ascDescBoolean))
+    #print(sorts.heapSort(numerosAleatorios,ascDescBoolean))
     print(sorts.mergeSort(numerosAleatorios,ascDescBoolean))
-    print(sorts.radixSort(numerosAleatorios,ascDescBoolean))
-    print(sorts.selectionSort(numerosAleatorios,ascDescBoolean))
-    print(sorts.shellSort(numerosAleatorios,ascDescBoolean))
+    #print(sorts.radixSort(numerosAleatorios,ascDescBoolean))
+    #print(sorts.selectionSort(numerosAleatorios,ascDescBoolean))
+    #print(sorts.shellSort(numerosAleatorios,ascDescBoolean))
 
