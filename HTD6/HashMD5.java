2ced85febb7ff3800ec10bb3118ae1f458e8b81a
Luis Pedro Lira <luispelirak130@gmail.com>
2024-03-14 11:14:25 -0600

    Clases implementaciones IHash

---
+++ HTD6/HashMD5.java
@@ -0,0 +1,11 @@
+package HTD6;
+
+public class HashMD5 implements IHash{
+
+    @Override
+    public String generateHash(String value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'generateHash'");
+    }
+    
+}
