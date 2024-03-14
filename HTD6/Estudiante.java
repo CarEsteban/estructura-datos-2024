c0154beae00d01b50a430b4b545b020fa87655e4
CarEsteban <car23016@uvg.edu.gt>
2024-03-14 11:41:11 -0600

    class Estudiante

---
+++ HTD6/Estudiante.java
@@ -0,0 +1,49 @@
+public class Estudiante {
+    private String name;
+    private String phone;
+    private String email;
+    private String postalZip;
+    private String country;
+
+    // Constructor
+    public Estudiante(String name, String phone, String email, String postalZip, String country) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.postalZip = postalZip;
+        this.country = country;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPostalZip() {
+        return postalZip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+    
+    // Método toString para imprimir la información del estudiante
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+               "name='" + name + '\'' +
+               ", phone='" + phone + '\'' +
+               ", email='" + email + '\'' +
+               ", postalZip='" + postalZip + '\'' +
+               ", country='" + country + '\'' +
+               '}';
+    }
+}
