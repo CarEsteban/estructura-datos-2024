c712484d91f72daf1fd75a351d70dbde5fc0a565
Luis Pedro Lira <luispelirak130@gmail.com>
2024-03-14 12:41:31 -0600

    LectorArchivo class

--- HTD6/HashSHA_1.java
+++ HTD6/HashSHA_1.java
@@ -5,25 +5,19 @@ public class HashSHA_1 implements IHash {
     @Override
     public String generateHash(String value) {
         try {
-            // Crear instancia de MessageDigest para SHA-1
             MessageDigest digest = MessageDigest.getInstance("SHA-1");
 
-            // Actualizar el digest utilizando el valor de entrada en bytes
             digest.update(value.getBytes());
 
-            // Completar el cálculo del hash
             byte[] bytes = digest.digest();
 
-            // Convertir el hash byte a byte en hexadecimal
             StringBuilder sb = new StringBuilder();
             for (byte b : bytes) {
                 sb.append(String.format("%02x", b));
             }
 
-            // Devolver el hash en formato hexadecimal
             return sb.toString();
         } catch (NoSuchAlgorithmException e) {
-            // En caso de que el algoritmo SHA-1 no esté disponible
             throw new RuntimeException(e);
         }
     }
---
+++ HTD6/LectorArchivo.java
@@ -0,0 +1,44 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.Map;
+
+public class LectorArchivo {
+    private FactoryMaps<String, Estudiante> factoryMaps;
+
+    public LectorArchivo(FactoryMaps<String, Estudiante> factoryMaps) {
+        this.factoryMaps = factoryMaps;
+    }
+
+    public void leerArchivo(String filePath, AbstractMap<String, Estudiante> estudiantesMap) {
+        JSONParser parser = new JSONParser();
+
+        try (FileReader reader = new FileReader(filePath)) {
+            Object obj = parser.parse(reader);
+
+            JSONArray estudiantes = (JSONArray) obj;
+
+            estudiantes.forEach(estudiante -> parseEstudianteObject((JSONObject) estudiante, estudiantesMap));
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    private void parseEstudianteObject(JSONObject estudiante, AbstractMap<String, Estudiante> estudiantesMap) {
+        String name = (String) estudiante.get("name");
+        String phone = (String) estudiante.get("phone");
+        String email = (String) estudiante.get("email");
+        String postalZip = (String) estudiante.get("postalZip");
+        String country = (String) estudiante.get("country");
+
+        Estudiante newEstudiante = new Estudiante(name, phone, email, postalZip, country);
+        estudiantesMap.put(name, newEstudiante);
+    }
+}
