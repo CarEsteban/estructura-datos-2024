3405bab7f8980a17a7800f712709a0b85b5e19f4
CarEsteban <car23016@uvg.edu.gt>
2024-04-04 13:07:22 -0600

    funcionamiento de los arboles

--- HTD7/src/Driver.java
+++ HTD7/src/Driver.java
@@ -1,31 +1,53 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
 
-public class Driver{
-    public static void main(String[] args) {
-        
-        //ITree englishTree = new BinarySearchTree<String,String>(Comparator<String> comparator);
+public class Driver {
+
+    public static void main(String[] args) throws IOException {
+        String fileName = "./src/diccionario.txt";
+        ArrayList<ArrayList<String>> dictionary;
+        //String fileName = "C:\\Users\\esteb\\OneDrive\\Documentos\\UNIVERSIDAD\\DATOS\\estructura-datos-2024\\HTD7\\src\\diccionario.txt";
+        //String directorioActual = System.getProperty("user.dir");
         ITree<String, ArrayList<String>> englishTree = new BinarySearchTree<>(Comparator.comparing(String::toString));
+        ITree<String, ArrayList<String>> spanishTree = new BinarySearchTree<>(Comparator.comparing(String::toString));
+        ITree<String, ArrayList<String>> frenchTree = new BinarySearchTree<>(Comparator.comparing(String::toString));
 
+        dictionary = loadDictionary(fileName);
+
+        
+        for (int i = 0; i < dictionary.size(); i++) {
+            englishTree.insert(dictionary.get(i).get(0), dictionary.get(i));
+            spanishTree.insert(dictionary.get(i).get(1), dictionary.get(i));
+            frenchTree.insert(dictionary.get(i).get(2), dictionary.get(i));
+        }
+
+        
+        
 
     }
 
-    public void loadDictionary(String fileName, BinarySearchTree englishTree, BinarySearchTree spanishTree, BinarySearchTree frenchTree) {
-        try (Scanner scanner = new Scanner(new File(fileName))) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] words = line.split(",");
-                Association association = new Association(words[0], words[1], words[2]);
-                
-                englishTree.insert(words[0], association);
-                spanishTree.insert(words[1], association);
-                frenchTree.insert(words[2], association);
+    public static ArrayList<ArrayList<String>> loadDictionary(String fileName) throws IOException {
+        ArrayList<ArrayList<String>> dictionary = new ArrayList<>();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+            String linea = reader.readLine();
+            while (linea != null) {
+                ArrayList<String> lineArray = new ArrayList<>(Arrays.asList(linea.split(","))); // Asumiendo coma como delimitador
+                dictionary.add(lineArray);
+                linea = reader.readLine();
             }
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
         }
+        return dictionary;
     }
 }
