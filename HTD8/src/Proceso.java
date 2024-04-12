58d9ca69aa57062974162bec626db55d382d667a
CarEsteban <car23016@uvg.edu.gt>
2024-04-11 20:27:44 -0600

    clase para procesos y factory del heap

---
+++ HTD8/src/Proceso.java
@@ -0,0 +1,30 @@
+public class Proceso  {
+    String nombreProceso;
+    String usuario;
+    int niceValue;
+    int prioridad; // PR calculado a partir del niceValue
+
+    // Constructor, getters, setters, etc.
+    public Proceso(String nombreProceso, String usuario, int niceValue){
+        this.nombreProceso = nombreProceso;
+        this.usuario = usuario;
+        this.niceValue = niceValue;
+        this.prioridad = 120 + niceValue;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+    
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public int getNiceValue() {
+        return niceValue;
+    }
+
+    public String getNombreProceso() {
+        return nombreProceso;
+    }
+}
