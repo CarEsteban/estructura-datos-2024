6e7f6c480dd5f50f06316e7e35897eaea7b14e88
CarEsteban <car23016@uvg.edu.gt>
2024-04-11 20:43:30 -0600

    leer Archivo

--- HTD8/src/Driver.java
+++ HTD8/src/Driver.java
@@ -1,5 +1,59 @@
 public class Driver{
     public static void main(String[] args) {
-        
+        String fileName = "./src/files/procesos.txt";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+
+    public static void leerEIngresarArchivo(Object tipoDeArbol, String fileName){
+
     }
 }
