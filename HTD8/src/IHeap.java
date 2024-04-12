e8a8815a19f69a4191525da14021ee89ffcf9642
Luis Pedro Lira <luispelirak130@gmail.com>
2024-04-11 19:40:43 -0600

    Heap Interface

---
+++ HTD8/src/IHeap.java
@@ -0,0 +1,17 @@
+/**
+ * @author moises.alonso
+ *
+ */
+public interface IHeap<P, V> {
+
+	void Insert(P priority, V value);
+	
+	V get();
+	
+	V remove();
+	
+	int count();
+	
+	boolean isEmpty();
+	
+}
