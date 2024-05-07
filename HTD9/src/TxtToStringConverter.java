6f74e4666b40e60dd2da784e7642d406ba602836
jlope384 <125939689+jlope384@users.noreply.github.com>
2024-05-07 13:49:49 -0600

    Convertidor de txt a string y archivo de texto

---
+++ HTD9/src/TxtToStringConverter.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class TxtToStringConverter {
+    public static String readFileToString(String filePath) throws IOException {
+        byte[] encodedBytes = Files.readAllBytes(Paths.get(filePath));
+        return new String(encodedBytes);
+    }
+}
