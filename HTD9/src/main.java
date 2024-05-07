a3ed79ce521c147d9848949146c548d570470029
CarEsteban <car23016@uvg.edu.gt>
2024-05-07 16:13:11 -0600

    texto a binario

--- HTD9/src/main.java
+++ HTD9/src/main.java
@@ -1,17 +1,29 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        // Crea una instancia de la clase Huffman con el texto a codificar
-        Huffman huffman = new Huffman("hola mi hermano");
+        // Ruta al archivo de texto
+        String filePath = "./src/texto_prueba.txt";
 
-        // Codifica el texto utilizando el árbol de Huffman
-        String encodedText = huffman.encode();
-        System.out.println("Texto codificado: " + encodedText);
+        try {
+            // Usa TxtToStringConverter para leer el contenido del archivo en una cadena
+            String text = TxtToStringConverter.readFileToString(filePath);
 
-        // Imprime los códigos generados para cada carácter
-        huffman.printCodes();
+            // Crea una instancia de la clase Huffman con el texto leído del archivo
+            Huffman huffman = new Huffman(text);
 
-        // Decodifica el texto codificado de regreso a su forma original
-        String originalText = huffman.decode(encodedText);
-        System.out.println("Texto original: " + originalText);
+            // Codifica el texto utilizando el árbol de Huffman
+            String encodedText = huffman.encode();
+            System.out.println("Texto codificado: " + encodedText);
+
+            // Imprime los códigos generados para cada carácter
+            huffman.printCodes();
+
+            // Decodifica el texto codificado de regreso a su forma original
+            //String originalText = huffman.decode(encodedText);
+            //System.out.println("Texto original: " + originalText);
+        } catch (IOException e) {
+            System.err.println("Error leyendo el archivo: " + e.getMessage());
+        }
     }
 }
