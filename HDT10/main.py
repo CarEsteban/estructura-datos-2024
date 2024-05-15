78c2c117804ec57a0ca8b7a7cc1cdfdd419e3062
CarEsteban <car23016@uvg.edu.gt>
2024-05-14 20:29:47 -0600

    menu para la matriz de adyacencia

--- HDT10/main.py
+++ HDT10/main.py
@@ -1,10 +1,7 @@
-# main.py
-
 from func import Graph, read_graph_from_file, print_adjacency_matrix, print_shortest_path, calculate_center_of_graph, modify_graph
 
 def main():
     filename = "logistica.txt"
-    # Lectura del archivo y construcción del grafo
     graph, cities = read_graph_from_file(filename)
 
     while True:
@@ -12,14 +9,13 @@ def main():
         print("1. Encontrar la ruta más corta entre 2 ciudades")
         print("2. Calcular el centro del grafo")
         print("3. Modificar el grafo")
-        print("4. Salir")
+        print("4. Mostrar matriz de adyacencia")
+        print("5. Salir")
         option = int(input("Ingrese una opción: "))
 
         if option == 1:
             city1 = input("Ingrese la ciudad de origen: ").strip()
             city2 = input("Ingrese la ciudad de destino: ").strip()
-
-            # Verificar que las ciudades ingresadas existen en la lista
             if city1 not in cities or city2 not in cities:
                 print("Una o ambas ciudades ingresadas no existen en la lista de ciudades.")
             else:
@@ -33,6 +29,9 @@ def main():
             modify_graph(graph, cities, filename)
 
         elif option == 4:
+            print_adjacency_matrix(graph)
+
+        elif option == 5:
             print("Saliendo del programa...")
             break
 
